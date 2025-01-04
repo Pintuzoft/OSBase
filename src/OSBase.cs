8aa949705c31f7e19474badccde80b77f6c847fd
DreamHealer <swehtc@gmail.com>
2025-01-04 11:44:09 +0000

    ...

--- src/OSBase.cs
+++ src/OSBase.cs
@@ -1,14 +1,29 @@
 ﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Events;
 
 namespace OSBase;
 public class OSBase : BasePlugin
 {
-    public override string ModuleName => "Hello World Plugin";
+    public override string ModuleName => "OSBase";
 
-    public override string ModuleVersion => "0.0.1";
+    public override string ModuleVersion => "0.0.2";
 
     public override void Load(bool hotReload)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("OSBase loaded!");
     }
+ 
+    [GameEventHandler]
+    private void OnMatchStart()
+    {
+        Console.WriteLine("Match started!");
+    }
+
+    [GameEventHandler]
+    private void OnMatchEnd()
+    {
+        Console.WriteLine("Match ended!");
+    }
+
 }
