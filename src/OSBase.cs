db0780d47db8ef8a32fb2032ff50cf0858c29b0a
DreamHealer <swehtc@gmail.com>
2025-03-02 01:48:07 +0000

    ...

--- src/OSBase.cs
+++ src/OSBase.cs
@@ -16,7 +16,7 @@ namespace OSBase;
 
 public class OSBase : BasePlugin {
     public override string ModuleName => "OSBase";
-    public override string ModuleVersion => "0.0.271";
+    public override string ModuleVersion => "0.0.272";
     public override string ModuleAuthor => "Pintuz";
     public override string ModuleDescription => "Plugin for handling map events with config execution";
     
